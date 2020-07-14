c5ff86db080fcf909aae8b7e566a43604008c309
Lucia <lesan2807@gmail.com>
2020-07-14 15:42:58 -0600

    Create new components: encabezado, pie, noticias, programa, agenda, mapa, calenddario, iniciativas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GeneralComponent } from './general/general.component';
+import { EncabezadoComponent } from './general/encabezado/encabezado.component';
+import { PieComponent } from './general/pie/pie.component';
+import { NoticiasComponent } from './Inicio/noticias/noticias.component';
+import { ProgramaComponent } from './Inicio/programa/programa.component';
+import { AgendaComponent } from './Inicio/agenda/agenda.component';
+import { MapaComponent } from './Inicio/mapa/mapa.component';
+import { CalendarioComponent } from './Noticias/calendario/calendario.component';
+import { IniciativasComponent } from './Iniciativas/iniciativas/iniciativas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GeneralComponent,
+    EncabezadoComponent,
+    PieComponent,
+    NoticiasComponent,
+    ProgramaComponent,
+    AgendaComponent,
+    MapaComponent,
+    CalendarioComponent,
+    IniciativasComponent
   ],
   imports: [
     BrowserModule,
