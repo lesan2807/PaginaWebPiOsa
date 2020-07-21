c4d56ed663c124dd77b019b6c0a97f881fec0d38
Lucia <lesan2807@gmail.com>
2020-07-21 16:27:38 -0600

    Finish encabezado component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { GeneralComponent } from './general/general.component';
-import { EncabezadoComponent } from './general/encabezado/encabezado.component';
-import { PieComponent } from './general/pie/pie.component';
-import { NoticiasComponent } from './Inicio/noticias/noticias.component';
+import { EncabezadoComponent } from './General/encabezado/encabezado.component';
+import { PieComponent } from './General/pie/pie.component';
 import { ProgramaComponent } from './Inicio/programa/programa.component';
-import { AgendaComponent } from './Inicio/agenda/agenda.component';
 import { MapaComponent } from './Inicio/mapa/mapa.component';
 import { CalendarioComponent } from './Noticias/calendario/calendario.component';
+import { AgendaComponent } from './Noticias/agenda/agenda.component';
+import { NoticiasComponent } from './Noticias/noticias/noticias.component';
 import { IniciativasComponent } from './Iniciativas/iniciativas/iniciativas.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    GeneralComponent,
     EncabezadoComponent,
     PieComponent,
     NoticiasComponent,
