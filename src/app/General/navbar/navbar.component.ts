88b70ef80401a8f68708c99d8fcdb4e80c1306eb
Lucia Elizondo <lesan2807@gmail.com>
2020-07-30 21:15:07 -0600

    Add navbar responsive

--- src/app/General/navbar/navbar.component.ts
+++ src/app/General/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public isCollapsed = true; 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
   }
 
 }
